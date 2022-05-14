6e8d2ceb5f3151be213425b0a720d64d75355f7a
acamarena88 <ag.camarena@gmail.com>
2022-05-14 17:39:12 -0500

    Added view

--- restapi/models.py
+++ restapi/models.py
@@ -1,9 +1,10 @@
 from django.db import models
 
-
 # Create your models here.
+
+
 class Expense(models.Model):
-    amount = models.FloatField
+    amount = models.FloatField()
     merchant = models.CharField(max_length=255)
     description = models.CharField(max_length=255, blank=True, null=True)
     category = models.CharField(max_length=255)
